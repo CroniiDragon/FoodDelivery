4fa85b964e5da68b0b9f81aaac95375ca438bbe4
Edward <paulescuedward@gmail.com>
2026-03-11 14:22:11 +0200

    Migrations

---
+++ FoodDelivery.OrderService/Migrations/20260311091357_AddPaymentFields.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDelivery.OrderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentReceipt",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PaymentSuccess",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentReceipt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentSuccess",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionId",
+                table: "Orders");
+        }
+    }
+}
